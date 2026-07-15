65a83d49c886305d444742c32beb7ccf8e459285
Yugam Grover <yugamgrover13@gmail.com>
2026-07-15 17:53:11 +0530

    setup express server and socket.io connection

---
+++ src/index.js
@@ -0,0 +1,20 @@
+const path = require("path");
+const express = require("express");
+const http = require("http");
+const socketio = require("socket.io");
+
+const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
+
+const PORT = process.env.PORT || 3000;
+const publicDirectoryPath = path.join(__dirname, "../public");
+
+app.use(express.static(publicDirectoryPath));
+
+io.on("connection", () => {
+  console.log("Websocket connection connected!");
+});
+server.listen(PORT, () => {
+  console.log("Server is up and running on port:", PORT);
+});
