a203c218f9a4406f462d3f261af432807faf4b9d
Yugam Grover <yugamgrover13@gmail.com>
2026-07-16 18:11:35 +0530

    implemented connection between client-server and server-client for real time messaging using socket events and realtime location send connection, styled the chat page and room join page.

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,58 @@
-io();
+const socket = io();
+
+const $messageForm = document.querySelector("#form1");
+const $messageFormInput = document.querySelector("input");
+const $messageFormButton = document.querySelector("button");
+const $locationButton = document.querySelector("#send-location");
+const $messages = document.querySelector("#messages");
+const messageTemplate = document.querySelector("#message-template").innerHTML;
+const locationTemplate = document.querySelector("#location-template").innerHTML;
+
+socket.on("message", (message) => {
+  const html = Mustache.render(messageTemplate, {
+    message: message.content,
+    createdAt: moment(message.createdAt).format("h:mm a"),
+  });
+  $messages.insertAdjacentHTML("beforeend", html);
+});
+
+socket.on("locationMessage", (url) => {
+  const html = Mustache.render(locationTemplate, {
+    url: url.content,
+    createdAt: moment(url.createdAt).format("h:mm a"),
+  });
+  $messages.insertAdjacentHTML("beforeend", html);
+});
+
+$messageForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  $messageFormButton.setAttribute("disabled", "disabled");
+
+  socket.emit("sendMessage", e.target.elements.message.value, () => {
+    $messageFormButton.removeAttribute("disabled");
+    $messageFormInput.value = "";
+    $messageFormInput.focus();
+
+    console.log("message delivered!");
+  });
+});
+
+$locationButton.addEventListener("click", () => {
+  if (!navigator.geolocation)
+    return console.log("location service is not supported by your browser.");
+  $locationButton.setAttribute("disabled", "disabled");
+
+  navigator.geolocation.getCurrentPosition((position) => {
+    socket.emit(
+      "sendLocation",
+      {
+        lat: position.coords.latitude,
+        lon: position.coords.longitude,
+      },
+      () => {
+        $locationButton.removeAttribute("disabled");
+        console.log("Location Shared!");
+      },
+    );
+  });
+});
--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ const path = require("path");
 const express = require("express");
 const http = require("http");
 const socketio = require("socket.io");
-
+const { generateMessage } = require("./utils/messages");
 const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
@@ -12,8 +12,27 @@ const publicDirectoryPath = path.join(__dirname, "../public");
 
 app.use(express.static(publicDirectoryPath));
 
-io.on("connection", () => {
+io.on("connection", (socket) => {
   console.log("Websocket connection connected!");
+
+  socket.emit("message", generateMessage("Welcome!"));
+  socket.on("sendMessage", (message, callback) => {
+    io.emit("message", generateMessage(message));
+    callback();
+  });
+  socket.broadcast.emit("message", generateMessage("a new user has joined."));
+  socket.on("sendLocation", (location, callback) => {
+    io.emit(
+      "locationMessage",
+      generateMessage(
+        `https://google.com/maps?q=${location.lat},${location.lon}`,
+      ),
+    );
+    callback();
+  });
+  socket.on("disconnect", () => {
+    io.emit("message", generateMessage("a user has left."));
+  });
 });
 server.listen(PORT, () => {
   console.log("Server is up and running on port:", PORT);
